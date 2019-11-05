01582466ba562cbefdc9228496f3d9de36920a59
pilasvgo <vinicius.pilas@gmail.com>
2019-11-05 16:43:41 -0300

    App com Identity
    App usando login com Identity.
    Apos executar migration executar arquivo "usuario inicial ADMIN.sql"

---
+++ Core_RBS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+
+namespace Core_RBS.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<RegisterModel> _logger;
+        //private readonly IEmailSender _emailSender;
+        
+
+
+        public RegisterModel(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            ILogger<RegisterModel> logger,            
+            RoleManager<IdentityRole> roleManager
+            //IEmailSender emailSender
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _roleManager = roleManager;
+            //_emailSender = emailSender;            
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "O campo e-mail é obrigatório", AllowEmptyStrings = false)]
+            [EmailAddress(ErrorMessage = "Formato de e-mail inválido")]
+            [Display(Name = "E-mail")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "O campo senha é obrigatório")]
+            [StringLength(100, ErrorMessage = "A {0} deve ter pelo menos {2} e no máximo {1} caracteres.", MinimumLength = 6)]            
+            [RegularExpression(@"^.*(?=.{5,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", ErrorMessage = "Senha fora do padrão, Ex: senhaIFS@123")]
+            [DataType(DataType.Password)]
+            [Display(Name = "Senha")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirmar senha")]
+            [Compare("Password", ErrorMessage = "O campo de confirmação de senha não confere!")]
+            public string ConfirmPassword { get; set; }
+            
+            [Display(Name = "Regra")]
+            [Required(ErrorMessage = "O campo Regra é obrigatório", AllowEmptyStrings = false)]
+            public string Name { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            // pass the Role List using ViewData
+            ViewData["roles"] = _roleManager.Roles.ToList();
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            // search role
+            var role = _roleManager.FindByIdAsync(Input.Name).Result;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    // code for adding user to role
+                    await _userManager.AddToRoleAsync(user, role.Name);
+                    // ends here
+
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { area = "Identity", userId = user.Id, code = code },
+                    //    protocol: Request.Scheme);
+
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    //if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    //{
+                    //    return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
+                    //}
+                    //else
+                    //{
+                    //    await _signInManager.SignInAsync(user, isPersistent: false);
+                    //    return LocalRedirect(returnUrl);
+                    //}
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                    
+                    
+
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
---
+++ Core_RBS/Models/Voto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace Core_RBS.Models
+{
+    public class Voto
+    {
+        [Key]
+        public long VotID { get; set; }
+        [DefaultValue(-1)]
+        public int Nota { get; set; }
+        [Display(Name = "Comentario:")]
+        public string Comentario { get; set; }
+        public int CamId { get; set; }
+        public DateTime DataVoto { get; set; }
+        public Campanha Campanha { get; set; }
+    }
+}
