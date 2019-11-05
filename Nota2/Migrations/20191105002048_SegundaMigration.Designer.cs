1ef1b1d6bbef1c108ee5e287a29aebdb6fe2249e
Willian Farias <williansantos_141@hotmail.com>
2019-11-04 21:26:28 -0300

    Close #13

--- Nota2/Migrations/20191105002048_SegundaMigration.Designer.cs
+++ Nota2/Migrations/20191105002048_SegundaMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nota2.Data;
 
 namespace Nota2.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20191105002048_SegundaMigration")]
+    partial class SegundaMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
