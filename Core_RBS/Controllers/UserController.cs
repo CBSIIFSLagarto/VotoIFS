2f4a41c6b25cb2f328b2707792e0cbc874200653
pilasvgo <vinicius.pilas@gmail.com>
2019-11-11 22:53:23 -0300

    Professor + Admin
    Usuario pode ser professor e admin, modelo simples

--- Core_RBS/Controllers/UserController.cs
+++ Core_RBS/Controllers/UserController.cs
@@ -9,12 +9,14 @@ namespace Core_RBS.Controllers
 {
     [Authorize(Policy = "Administrador")]
     public class UserController : Controller
-    {        
-        private readonly UserManager<Usuario> _userManager;       
-        public UserController(UserManager<Usuario> userManager)
+    {
+        private readonly UserManager<Usuario> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;        
+        public UserController(UserManager<Usuario> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
-            
+            _roleManager = roleManager;
+
         }
         // GET: Campanhas        
         public IActionResult Index()
@@ -28,12 +30,48 @@ namespace Core_RBS.Controllers
             var token = await _userManager.GeneratePasswordResetTokenAsync(user);
             var result = await _userManager.ResetPasswordAsync(user, token, "ifs@123");
             if (result.Succeeded)
-            {                
+            {
                 TempData["Success"] = "Senha resetada com sucesso, nova senha: ifs@123";
             }
             else
             {
                 TempData["Success"] = "Ocorreu algum erro, entrar contato com administrador.";
+            }
+
+            return RedirectToAction("Index", "User");
+        }
+        public async Task<IActionResult> RoleUpdate(string id)
+        {
+            Usuario user = await _userManager.FindByNameAsync(id);
+            var role = _roleManager.FindByNameAsync("Administrador").Result;
+            if (role != null)
+            {
+                IdentityResult result;
+                var isAdmin = await _userManager.IsInRoleAsync(user, role.Name);
+                if (isAdmin)
+                {
+                    result = await _userManager.RemoveFromRoleAsync(user, role.Name);
+                    if (result.Succeeded)
+                    {
+                        TempData["Success"] = "Você removeu o usuário "+ user.Nome +" do grupo de "+role.Name;
+                    }
+                    else
+                    {
+                        TempData["Success"] = "Ocorreu algum erro, entrar contato com administrador.";
+                    }
+                }
+                else
+                {
+                    result = await _userManager.AddToRoleAsync(user, role.Name);
+                    if (result.Succeeded)
+                    {
+                        TempData["Success"] = "Você adicionou o usuário " + user.Nome + " ao grupo de " + role.Name;
+                    }
+                    else
+                    {
+                        TempData["Success"] = "Ocorreu algum erro, entrar contato com administrador.";
+                    }
+                }
             }
 
             return RedirectToAction("Index", "User");
