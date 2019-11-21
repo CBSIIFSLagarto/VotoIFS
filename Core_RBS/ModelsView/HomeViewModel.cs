9fb92ebad236352c2f6783ddf4b6de888ecd3a27
Willian Farias <williansantos_141@hotmail.com>
2019-11-21 00:21:49 -0300

    Close #30

---
+++ Core_RBS/ModelsView/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Core_RBS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_RBS.ModelsView
+{
+    public class HomeViewModel
+    {
+        public ICollection<Campanha> Campanhas { get; set; }
+    }
+}
