5a19ce14a9b8ada66f51eb4a4d800c672b58a691
Willian Farias <williansantos_141@hotmail.com>
2019-11-26 22:43:19 -0300

    Close #36

--- Core_RBS/Models/Campanha.cs
+++ Core_RBS/Models/Campanha.cs
@@ -16,7 +16,7 @@ namespace Core_RBS.Models
 
         [Display(Name = "Chave de acesso")]
         //[Required(ErrorMessage = "Chave de acesso é obrigatória", AllowEmptyStrings = false)]
-        [StringLength(20, MinimumLength = 4)]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "A chave de acesso deve conter no mínimo 4 e máximo 20 caracteres.")]
         public String Chave
         {
             get
